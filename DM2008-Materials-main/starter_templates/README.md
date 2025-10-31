25ec72c427d41ebf6714a646df0333b6b9d8cb39
lumiestorey <emil0021@e.ntu.edu.sg>
2025-10-31 13:58:14 +0800

    starter templates

---
+++ DM2008-Materials-main/starter_templates/README.md
@@ -0,0 +1,9 @@
+# Starter Templates
+
+## p5.js Only Templates
+
+Instructions coming soon.
+
+## p5.js with Arduino Templates
+
+Instructions coming soon.
---
+++ DM2008-Materials-main/starter_templates/p5-only/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // If you want a canvas that fills the window
+  // use createCanvas(windowWidth, windowHeight)
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(240);
+}
